a408b4a540d2f8023d0032790a179edaca6f4209
ilyashaaa <86850598+ilyashaa@users.noreply.github.com>
2024-03-19 23:28:11 +0300

    add GET

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+Name string
+Email string
+Age int
+}
+
+
+
 func main() {
+
+users := make(map[string]User)
+
+
+
 	router := gin.Default()
 
 	router.GET("/author", func(c *gin.Context) {
@@ -16,15 +33,45 @@ func main() {
 		c.HTML(http.StatusOK, "register.html", gin.H{})
 	})
 
+	router.GET("/user", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "%s",users)
+	})
+
 	router.POST("/user", func(c *gin.Context) {
+		
 		name := c.PostForm("name")
 		email := c.PostForm("email")
-		age := c.PostForm("age")
-		response := "Получены данные: Имя - " + name + ", Email - " + email + ", Age - " + age
+		age, _ := strconv.Atoi(c.PostForm("age"))
+
+		user := User{
+			Name: name,
+			Email: email,
+			Age: age,
+		}
+        users[email] = user
+
+		response := fmt.Sprintf("Получены данные: Имя - %s , Email - %s, Age - %d ", name, email, age )
 		c.String(http.StatusOK, response)
 	})
 
-	router.LoadHTMLFiles("templates/author.html", "templates/register.html")
+	router.POST("/auth", func(c *gin.Context) {
+		
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+        user, ok := users[email]
+
+		if !ok || strings.Compare(user.Name, name) != 0 {
+			c.String(http.StatusForbidden, "%s","Ты лох педальный")
+			return
+		}
+
+		response := fmt.Sprintf("Получены данные: Имя - %s , Email - %s, Age - %d ", user.Name, user.Email, user.Age)
+		c.String(http.StatusOK, response)
+	})
+	
+
+	router.LoadHTMLGlob("templates/*")
 
 	router.Run(":8080")
 }
